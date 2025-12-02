8b5d8fe61c3090e63d6afeda3c2a2c326b55a920
testhype <chris676@gmail.com>
2025-12-02 19:52:21 +0200

    feat: add DivApp

---
+++ src/gr/aueb/cf/testgit/DivApp.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.testgit;
+
+public class DivApp {
+
+    static void main() {
+
+    }
+
+    public static int div(int a, int b) {
+        if (b == 0) return 0;
+        return a / b;
+    }
+}
